c40ab1ad8794a94e5d42e0c5075dcd0387129003
t0ng42013 <tonga88@live.com.ar>
2023-01-17 14:43:09 -0300

    back portfolio base

---
+++ src/main/java/com/portfolio/gastonAlonso/GastonAlonsoApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.gastonAlonso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastonAlonsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastonAlonsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/gastonAlonso/GastonAlonsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.gastonAlonso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastonAlonsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
