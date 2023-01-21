a988492e13f01f981c8ba73a2319d0154531e08d
t0ng42013 <tonga88@live.com.ar>
2023-01-21 01:27:18 -0300

    correciones a origins

--- src/main/java/com/portfolio/gastonAlonso/config/CorsConfig.java
+++ src/main/java/com/portfolio/gastonAlonso/config/CorsConfig.java
@@ -14,13 +14,13 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/login")
-                        .allowedOrigins("*")
+                        .allowedOrigins("https://lga-portfolio.web.app")
                         .allowedMethods("*")
                         .exposedHeaders("*");
 
 
                 registry.addMapping("/api/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("https://lga-portfolio.web.app")
                         .allowedMethods("*");
             }
 
